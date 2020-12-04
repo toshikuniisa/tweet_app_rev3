60338e4ce35b3ecfddd1fe18a9035d209b9f59ba
0084takuya <ba-bo-lat.ot-fn.109-110.5296@ezweb.ne.jp>
2020-12-04 13:39:05 +0900

    controllerのリファクタリング

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,19 @@
 class LikesController < ApplicationController
+  before_action :set_post
 
   def create
-     @like=Like.new(user_id: current_user.id,post_id:params[:post_id])
-     @like.save
-     redirect_to("/posts/#{params[:post_id]}")
+    @like = Like.create(user: current_user, post: @post)
+    redirect_to post_path(@post)
   end
 
   def destroy
-    @like=Like.find_by(user_id: current_user.id,post_id:params[:post_id])
+    @like = Like.find_by(user: current_user, post: @post)
     @like.destroy
-    redirect_to("/posts/#{params[:post_id]}")
+    redirect_to post_path(@post)
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
